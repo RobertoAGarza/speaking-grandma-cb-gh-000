35c5144345400746afd87987191e857533d4b98d
Roberto Garza <robertg4567@hotmail.com>
2018-07-17 18:10:53 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
   elsif phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
   end
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
 
 end
